9bd578abdd9103346bc2278581d4b3af4eb4944b
KazukiShimamoto <jakky0529@gmail.com>
2018-06-04 11:44:56 +0900

    change Main

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,8 +3,8 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		    String output = String.format("%s", args[i]);
+			System.out.println("Hello " + output + "!");
 		}
 	}
 }
